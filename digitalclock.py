f89600bfd17caa1e54966c9b1ab260a8c3f28b4a
Muntasir00 <muntasir690000@gmail.c.com>
2021-10-09 20:19:11 +0600

    digital clock python programs created

---
+++ digitalclock.py
@@ -0,0 +1,19 @@
+#Importing modules
+from tkinter import *
+from time import strftime
+
+root = Tk()  #Creates tkinter window
+root.title("Digital Computer Clock") #Adds title to tkinter window
+
+#Function usedto display timeon the label
+def time():
+    string = strftime("%H:%M:%S %p")
+    lbl.config(text=string)
+    lbl.after(1000, time)
+
+#Styling the label widget which displays the clock    
+lbl = Label(root, font = ("arial", 160, "bold"),bg="black",fg="white")
+#Pack method in tkinter packs widgets into rows or columns
+lbl.pack(anchor="center",fill="both",expand=1)
+time() #Time function is called
+mainloop() #Runs the application program
